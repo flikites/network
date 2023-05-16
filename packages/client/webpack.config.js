86c9ae19c1271535eb787805cf17ef8ed50cec19
Eric Andrews <harbu88@hotmail.com>
2023-05-16 16:06:02 +0300

    release(client): v8.5.0-nolit.1

--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -119,6 +119,8 @@ module.exports = (env, argv) => {
                 [path.resolve('./src/utils/persistence/ServerPersistence.ts')]: (
                     path.resolve('./src/utils/persistence/BrowserPersistence.ts')
                 ),
+                '@lit-protocol/lit-node-client': false,
+                'lit-siwe': false,
                 '@walletconnect/ethereum-provider': false, // This and below brought in by @litprotocol/client-node but not actually used by our use case...
                 '@walletconnect/universal-provider': false,
                 '@walletconnect/core': false,
